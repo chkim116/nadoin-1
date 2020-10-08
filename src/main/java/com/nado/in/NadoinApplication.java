548827ab81d7e59fe4ad882b5a3ab1d45cafaa1c
denyok1995 <denyok1995@gmail.com>
2020-10-08 17:00:08 +0900

    프로젝트 ide연동

---
+++ src/main/java/com/nado/in/NadoinApplication.java
@@ -0,0 +1,13 @@
+package com.nado.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NadoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nado/in/NadoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nado.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NadoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
