76326b665977c24ebbe62af1fdf10f02dd6d50e5
denyok1995 <denyok1995@gmail.com>
2020-10-08 19:02:23 +0900

    JDBC 설정무시 어노테이션 추가, url mapping 충돌 해결

--- src/main/java/com/nado/in/TestController.java
+++ src/main/java/com/nado/in/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-    @RequestMapping
+    @RequestMapping(path="/test")
     public String test(){
         return null;
     }
