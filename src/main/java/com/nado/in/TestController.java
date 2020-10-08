e3946558a328ffdd5553f9640125c667a022535e
denyok1995 <denyok1995@gmail.com>
2020-10-08 18:48:07 +0900

    React 추가

---
+++ src/main/java/com/nado/in/TestController.java
@@ -0,0 +1,12 @@
+package com.nado.in;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @RequestMapping
+    public String test(){
+        return null;
+    }
+}
